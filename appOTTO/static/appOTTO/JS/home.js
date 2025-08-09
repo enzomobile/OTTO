32384e5546372c97ee4052df42ab30c144f1b074
Angelo <angelo.souza23@etec.sp.gov.br>
2025-08-09 13:26:23 -0300

    09/08/2025 "Landing Page entegrada'

--- appOTTO/static/appOTTO/JS/home.js
+++ appOTTO/static/appOTTO/JS/home.js
@@ -1,3 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Django está com JavaScript funcionando!");
+document.querySelectorAll(".botao-navegar").forEach(function(botao) {
+    botao.addEventListener("click", function() {
+        window.location.href = this.dataset.url;
+    });
 });
+
+const carrosselInner = document.querySelector('.carrossel-inner');
+const items = document.querySelectorAll('.carrossel-inner .item');
+let currentIndex = 0;
+
+function updateSlide() {
+  const width = document.querySelector('.carrossel-viewport').offsetWidth;
+  carrosselInner.style.transform = `translateX(-${currentIndex * width}px)`;
+}
+
+// Avançar
+document.getElementById('next').addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateSlide();
+});
+
+// Voltar
+document.getElementById('prev').addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  updateSlide();
+});
+
+// Troca automática a cada 10s
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateSlide();
+}, 10000);
+
+// Ajustar ao redimensionar
+window.addEventListener('resize', updateSlide);
+
+// Inicializar
+updateSlide();
