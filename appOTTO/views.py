f164529947db19e3713b476c1b08a58031ae9211
Angelo <angelo.souza23@etec.sp.gov.br>
2025-09-08 17:00:12 -0300

    08/09/2025 "tela pos_fase finalizada"

--- appOTTO/views.py
+++ appOTTO/views.py
@@ -233,16 +233,16 @@ def fases(request):
     return render(request, 'appOTTO/fases.html')
 
 FASES = {
-    1: {"titulo": "Fase 1", "descricao": "Explicação da fase 1", "imagem": "img/fases/fase1.png"},
-    2: {"titulo": "Fase 2", "descricao": "Explicação da fase 2", "imagem": "img/fases/fase2.png"},
-    3: {"titulo": "Fase 3", "descricao": "Explicação da fase 3", "imagem": "img/fases/fase3.png"},
-    4: {"titulo": "Fase 4", "descricao": "Explicação da fase 4", "imagem": "img/fases/fase4.png"},
-    5: {"titulo": "Fase 5", "descricao": "Explicação da fase 5", "imagem": "img/fases/fase5.png"},
-    6: {"titulo": "Fase 6", "descricao": "Explicação da fase 6", "imagem": "img/fases/fase6.png"},
-    7: {"titulo": "Fase 7", "descricao": "Explicação da fase 7", "imagem": "img/fases/fase7.png"},
-    8: {"titulo": "Fase 8", "descricao": "Explicação da fase 8", "imagem": "img/fases/fase8.png"},
-    9: {"titulo": "Fase 9", "descricao": "Explicação da fase 9", "imagem": "img/fases/fase9.png"},
-    10: {"titulo": "Fase 10", "descricao": "Explicação da fase 10", "imagem": "img/fases/fase10.png"},
+    1: {"titulo": "Fase 1", "descricao": "Explicação da fase 1"},
+    2: {"titulo": "Fase 2", "descricao": "Explicação da fase 2"},
+    3: {"titulo": "Fase 3", "descricao": "Explicação da fase 3"},
+    4: {"titulo": "Fase 4", "descricao": "Explicação da fase 4"},
+    5: {"titulo": "Fase 5", "descricao": "Explicação da fase 5"},
+    6: {"titulo": "Fase 6", "descricao": "Explicação da fase 6"},
+    7: {"titulo": "Fase 7", "descricao": "Explicação da fase 7"},
+    8: {"titulo": "Fase 8", "descricao": "Explicação da fase 8"},
+    9: {"titulo": "Fase 9", "descricao": "Explicação da fase 9"},
+    10: {"titulo": "Fase 10", "descricao": "Explicação da fase 10"},
 }
 
 def pre_fase(request, numero):
